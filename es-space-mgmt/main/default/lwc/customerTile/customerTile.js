0bce8e158e75a4461a93d4317a1125bb3ab1fc80
Abhay Pathak <abyptk2@gmail.com>
2021-05-08 20:37:06 +0530

    exercise6

---
+++ es-space-mgmt/main/default/lwc/customerTile/customerTile.js
@@ -0,0 +1,16 @@
+import { LightningElement,api } from 'lwc';
+
+export default class CustomerTile extends LightningElement {
+    @api customer;
+    @api object;
+
+    connectedCallback(){}
+
+    get icon(){
+        return 'doctype:image';
+    }
+
+    get alttext(){
+        return 'image';
+    }
+}
